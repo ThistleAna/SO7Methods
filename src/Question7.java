4b8b42dbf070b5f6d96c0d7a7de742fb43025058
ANA KHASANAH <ana.khasanah@capita.com>
2023-04-20 13:16:28 +0100

    Exercises on Method, Method Parameter and Argument, Method Overloading.

--- src/Question7.java
+++ src/Question7.java
@@ -1,8 +1,8 @@
 public class Question7 {
     // Create a checkAge() method with an integer variable called age
-    static void	.....	 (........) {
+    static void	checkAge(int age) {
         // If age is less than 18, print "Access denied"
-       if (......... ) {
+       if (age < 18) {
                 System.out.println("Access denied");
         // If age is greater than, or equal to, 18, print "Access granted"
  	    } else{
@@ -13,7 +13,9 @@ public class Question7 {
 
     public static void main(String[] args) {
         // Call the checkAge method and pass along an age of 20
-       checkAge(20);
+       checkAge(20); // granted
+       checkAge(17); // denied
+       checkAge(13); // denied
     }
 
 }
